6d4ad4b7d84e7abf371d3f6457aedaad7e199dc0
Marwan KN <marwannofal@gmail.com>
2024-05-26 23:42:22 +0300

    "Refactor FeaturedProducts component: added container for text, updated styles for tablet layout, and adjusted image and button styling"

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -17,17 +17,19 @@ const FeaturedProducts = () => {
             alt="Speaker Image"
             className="featuredProducts-largeCard__image"
           />
-          <h3 className="featuredProducts-largeCard__title">ZX9 SPEAKER</h3>
-          <p className="featuredProducts-largeCard__text">
-            Upgrade to premium speakers that are phenomenally built to deliver
-            truly remarkable sound.
-          </p>
-          <Link
-            to={`/categories/speakers/zx9-speaker`}
-            className="featuredProducts-largeCard__button"
-          >
-            See Product
-          </Link>
+          <div className="featuredProducts-largeCard__text-container">
+            <h3 className="featuredProducts-largeCard__title">ZX9 SPEAKER</h3>
+            <p className="featuredProducts-largeCard__text">
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </p>
+            <Link
+              to={`/categories/speakers/zx9-speaker`}
+              className="featuredProducts-largeCard__button"
+            >
+              See Product
+            </Link>
+          </div>
         </div>
       </div>
     </section>
