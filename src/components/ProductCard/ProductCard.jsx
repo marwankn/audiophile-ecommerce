0bf82b38789bdbaf25de28333132f6466fcc646a
Marwan KN <marwannofal@gmail.com>
2024-06-01 21:07:09 +0300

    "Added useParams hook and updated CategoriesPage page to display category title from URL parameter"

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProductCard.scss";
+
+const ProductCard = () => {
+  return <div>ProductCard</div>;
+};
+
+export default ProductCard;
--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import "./CategoriesPage.scss";
 
 const CategoriesPage = () => {
-  return <div>CategoriesPage</div>;
+  const { category } = useParams();
+  return (
+    <>
+      <section className="categoriesPage__hero">
+        <div className="categoriesPage__hero-container">
+          <h4 className="categoriesPage__hero-title">{category}</h4>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default CategoriesPage;
