57d4ebfc178f6c57425cc1a12231d7de4769a506
Marwan KN <marwannofal@gmail.com>
2024-06-06 09:11:15 +0300

    "Updated image paths in data.json files to use '/public/assets/images/' instead of './assets/'; added ProductCategories and FeaturedAboutUs components to CategoriesPage.jsx"

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import data from "../../assets/data/data.json";
 import "./CategoriesPage.scss";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import ProductCategories from "../../components/ProductCategories/ProductCategories";
+import FeaturedAboutUs from "../../components/FeaturedAboutUs/FeaturedAboutUs";
 
 const CategoriesPage = () => {
   const { category } = useParams();
@@ -21,6 +23,8 @@ const CategoriesPage = () => {
           }
         })}
       </section>
+      <ProductCategories />
+      <FeaturedAboutUs />
     </>
   );
 };
